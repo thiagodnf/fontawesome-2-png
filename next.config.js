a9cfa059cd42066fd4b85bc8c7f83a688c1efea4
Thiago Ferreira <thiagodnf@gmail.com>
2022-10-23 09:24:17 -0400

    some improveemnts

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    basePath: "/fontawesome-2-png",
+    trailingSlash: true,
+    // images: {
+    //     loader: "akamai",
+    //     path: "",
+    // },
+    publicRuntimeConfig: {
+        basePath: "/fontawesome-2-png",
+    }
+};
